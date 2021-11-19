348c9931a2e9f3d5cb3751be4e9bb40a8743a51f
Katlyn Boches <katlynboches@gmail.com>
2021-11-18 19:21:59 -0700

    updated seeds and index.js

--- employees_db/seeds.sql
+++ employees_db/seeds.sql
@@ -7,13 +7,14 @@ VALUES ("Sales"),
        ("Legal");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ( "role.title", 100000.00, 1),
-       ( "role.title", 100000.00, 2),
-       ( "role.title", 100000.00, 3),
-       ( "role.title", 100000.00, 4);
+VALUES ( "Salesperson", 100000.00, 1),
+       ( "Accountant", 100000.00, 2),
+       ( "Software Engineer", 100000.00, 3),
+       ( "Mechanical Engineer", 100000.00, 4);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES 	("first_name", "last_name", 1, NULL),
-		("first_name", "last_name", 2, 1),
-        ("first_name", "last_name", 3, 2),
-        ("first_name", "last_name", 4, 3);
+VALUES 	("James", "Michales", 1, NULL),
+		("Aaron", "Samuels", 2, 1),
+        ("David", "Smith", 3, 2),
+        ("Marc", "Jacobs", 4, 3);
+
